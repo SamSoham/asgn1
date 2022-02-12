44e442f1eb0fba9a9f41caefe27efbd4cb4044ef
SamSoham <98308722+SamSoham@users.noreply.github.com>
2022-02-12 19:42:09 +0530

    Add files via upload

--- Q1.c
+++ Q1.c
@@ -1,21 +1,9 @@
 #include <stdio.h>
 int main(){
-	char name[20],hob[50],br[50];
-	int reg;
-	printf("Enter your name ");
-	gets(name);
-	printf("\nname of the branch ");
-	gets(br);
-    printf("\nenter your hobbies ");
-	gets(hob);
-    printf("\n enter your reg no ");
-    scanf("%d",&reg);
-	puts(name);
-	printf(" %d",reg);
-	printf("\n");
-	puts(br);
-	puts(hob);
-	
+printf("Name - Soham Samantaray\n");
+printf("Registartion num.- 035\n");
+printf("Branch- Computer Science and Engineering\n");
+printf("Hobbies- Playing outdoor games,watching MCU movies");
 	
 	return 0;
 }
